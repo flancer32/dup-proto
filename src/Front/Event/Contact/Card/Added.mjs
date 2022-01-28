c1b5b2edeec49d085a52cb788c04681111855550
Alex Gusev <alex@flancer64.com>
2022-01-28 09:16:19 +0200

    DC-63 Clean up deprecated TeqFw_Core_Shared_Api_IEvent

--- src/Front/Event/Contact/Card/Added.mjs
+++ src/Front/Event/Contact/Card/Added.mjs
@@ -13,16 +13,40 @@ class Dto {
 }
 
 /**
- * @implements TeqFw_Core_Shared_Api_IEvent
+ * @implements TeqFw_Core_Shared_Api_Factory_Dto_IEvent
  */
 export default class Fl32_Dup_Front_Event_Contact_Card_Added {
-    constructor() {
+    constructor(spec) {
+        // EXTRACT DEPS
+        /** @type {TeqFw_Core_Shared_App_Event_Message} */
+        const dtoBase = spec['TeqFw_Core_Shared_App_Event_Message$'];
+
+        // ENCLOSED VARS
+        const ATTR = dtoBase.getAttributes();
+
+        // ENCLOSED FUNCTIONS
         /**
-         * @param [data]
+         * @param {Fl32_Dup_Front_Event_Contact_Card_Added.Dto} [data]
          * @return {Fl32_Dup_Front_Event_Contact_Card_Added.Dto}
          */
-        this.createDto = (data) => new Dto();
+        function createData(data) {
+            const res = new Dto();
+            return res;
+        }
 
-        this.getName = () => NS;
+        // INSTANCE METHODS
+        /**
+         * @param {{[data]: Fl32_Dup_Front_Event_Contact_Card_Added.Dto, [meta]: TeqFw_Core_Shared_App_Event_Message_Meta.Dto}} [data]
+         * @return {{data: Fl32_Dup_Front_Event_Contact_Card_Added.Dto, meta: TeqFw_Core_Shared_App_Event_Message_Meta.Dto}}
+         */
+        this.createDto = function (data) {
+            const res = dtoBase.createDto({[ATTR.META]: data?.[ATTR.META]});
+            res.meta.name = NS;
+            res.data = createData(data?.[ATTR.DATA]);
+            // noinspection JSValidateTypes
+            return res;
+        }
+
+        this.getEventName = () => NS;
     }
 }
