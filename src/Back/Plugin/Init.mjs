6413ba80a934559a4a1e816cbda6418f3bbb6561
Alex Gusev <alex@flancer64.com>
2022-03-11 08:23:53 +0200

    DC-97 Restore github actions

--- src/Back/Plugin/Init.mjs
+++ src/Back/Plugin/Init.mjs
@@ -45,6 +45,7 @@ export default function Factory(spec) {
                 } catch (error) {
                     await trx.rollback();
                 }
+                await conn.disconnect(); // to prevent nodejs freezing on 'stop' commands
                 return res;
             }
 
